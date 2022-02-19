a4289eceae08365b8cd699979774e6b7ce0c9a2f
Frozencode <1254068357@qq.com>
2022-02-19 11:41:08 +0800

    add a new file

---
+++ test1.cpp
@@ -0,0 +1,18 @@
+/*
+	Author:Frozencode
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+typedef long long ll;
+typedef pair<ll,ll> pii;
+const ll maxn = 500010;
+const ll INF = 2147483647;
+int main()
+{ 
+	ios::sync_with_stdio(false);
+	return 0;
+}
